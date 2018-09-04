54f40340b1fc89ef8cc6f618c1df4e49ef36c6dc
liuwei <lliuwei1185032659@gmail.com>
2018-09-04 23:05:50 +0800

    添加原型链章节

---
+++ coding/JavaScript基础一/原型/定义/index.js
@@ -0,0 +1,4 @@
+// 构造函数首字母大写
+function Persion() {}
+
+console.dir(Persion)
---
+++ coding/JavaScript基础一/原型/继承/index.js
@@ -0,0 +1,10 @@
+// 构造函数首字母大写
+function Persion() {}
+
+var xiaoming = new Persion()
+
+console.log('%cPersion构造函数：', 'color: red;')
+console.dir(Persion)
+
+console.log('%cxiaoming实例：', 'color: red;')
+console.dir(xiaoming)
